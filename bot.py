f64015f5739addf6426627a59f04a4ab3cc60716
KValexander <ewoase@gmail.com>
2022-03-23 16:21:14 +0300

    v1.0 upd

--- bot.py
+++ bot.py
@@ -2,19 +2,20 @@
 import requests
 import json
 import time
-import sys
+import threading
 
 # Api
 api = {
     "url": "",
     "api":  "https://api.telegram.org/bot",
     "token": "",
-    "channel": "",
+    "publication_chat_id": 0,
     "user_id": 0,
     "chat_id": 0,
     "update_id": 0,
     "update_limit": 30,
     "current_command": None,
+    "timer": None,
     "photos": [],
 }
 # Url
@@ -37,8 +38,6 @@ def getUpdates():
 
     # get = api["url"] + commands["getUpdates"]
     response = requests.get(get).json()
-    api["user_id"] = response["result"][-1]["message"]["from"]["id"]
-    api["chat_id"] = response["result"][-1]["message"]["chat"]["id"]
     return response
 
 # Get command
@@ -49,17 +48,21 @@ def getCommand():
 
     # Getting the last object
     update_obj = response[-1]
-
-    # Command processing
-    if api["current_command"]:
-        commandProcessing(update_obj, response)
+    api["user_id"] = update_obj["message"]["from"]["id"]
+    api["chat_id"] = update_obj["message"]["chat"]["id"]
 
     # Check update_id
     if api["update_id"] == 0:
         api["update_id"] = update_obj["update_id"]
 
-    # Command return
+    # Getting only the next message
     if update_obj["update_id"] != api["update_id"]:
+
+        # Command processing
+        if api["current_command"]:
+            commandProcessing(update_obj, response)
+
+        # Command return
         if "text" in update_obj["message"]:
             api["update_id"] = update_obj["update_id"]
             return update_obj["message"]["text"]
@@ -87,6 +90,19 @@ def commandProcessing(update_obj, response):
             # Success message
             sendMessage(api["chat_id"], f"{text}.\nДобавленные изображения можно посмотреть с помощью команды \"/view\"")
 
+    # Publish images after a certain time
+    elif api["current_command"] == "publication":
+        if "text" in update_obj["message"]:
+            api["current_command"] = None
+            time = update_obj["message"]["text"]
+            if time.isdigit():
+                time = int(time)
+                sendMessage(api["chat_id"], f"Добавленные изображения будут публиковаться раз в {time} секунд")
+                api["timer"] = threading.Timer(time, publicationTime, (api["publication_chat_id"], api["photos"], time,))
+                api["timer"].start()
+            else: sendMessage(api["chat_id"], "Неверный формат")
+
+
 # Add photo
 def addPhoto(file_id):
     index = len(api["photos"]) - 1
@@ -105,17 +121,33 @@ def addPhoto(file_id):
     })
 
 # Out photos
-def outPhotos(array):
+def outPhotos(chat_id, array):
     if len(array) == 0: return False
 
     count = 0
     for photos in api["photos"]:
-        sendMediaGroup(api["chat_id"], json.dumps(photos.copy()))
+        sendMediaGroup(chat_id, json.dumps(photos.copy()))
         count += len(photos)
 
-    sendMessage(api["chat_id"], f"Всего изображений: {count}")
+    if api["chat_id"] == chat_id:
+        sendMessage(chat_id, f"Всего изображений: {count}")
 
     return True
+
+# Publication by time
+def publicationTime(chat_id, array, time):
+    if len(array) == 0:
+        sendMessage(api["chat_id"], "Изображения отсутствуют, публикация остановлена")
+        return
+
+    photo = array[0].pop(0)
+    sendPhoto(chat_id, photo["media"])
+
+    if len(array[0]) == 0:
+        array.pop(0)
+
+    api["timer"] = threading.Timer(time, publicationTime, (chat_id, array, time,))
+    api["timer"].start()
 
 # Send message
 def sendMessage(chat_id, text):
@@ -153,19 +185,37 @@ while True:
         if command == "/start":
             sendMessage(api["chat_id"], "Добро пожаловать. Выберите интересующее вас действие, введя в поле ввода \"/\"")
         
-        # Upload image
+        # Upload images
         elif command == "/upload":
             api["current_command"] = "upload"
             sendMessage(api["chat_id"], "Вставьте изображение (не более 10 за раз для корректного добавления)")
 
         # View images
         elif command == "/view":
-            if not outPhotos(api["photos"]):
-                sendMessage(api["chat_id"], f"Добавленные изображения отсутствуют")
+            if not outPhotos(api["chat_id"], api["photos"]):
+                sendMessage(api["chat_id"], "Добавленные изображения отсутствуют")
 
         # Delete images
         elif command == "/clear":
             api["photos"].clear()
-            sendMessage(api["chat_id"], f"Список добавленных изображений очищен")
+            sendMessage(api["chat_id"], "Список добавленных изображений очищен")
+
+        # Publish images
+        elif command == "/publish":
+            outPhotos(api["publication_chat_id"], api["photos"])
+            api["photos"].clear()
+            sendMessage(api["chat_id"], "Добавленные изображения были опубликованы и удалены из списка")
+
+        # Publish images after a certain time
+        elif command == "/publication_time":
+            api["current_command"] = "publication"
+            sendMessage(api["chat_id"], "Введите через какое количество секунд будут выкладываться изображения (только целые числа)")
+
+        # Stop publishing
+        elif command == "/stop_publication":
+            if api["timer"] != None:
+                api["timer"].cancel()
+                sendMessage(api["chat_id"], "Публикация остановлена")
+            else: sendMessage(api["chat_id"], "Публикация не запущена")
 
     time.sleep(1)
